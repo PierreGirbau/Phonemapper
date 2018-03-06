ab733d51155af9c3014446f52493e0c8c2269ed3
Noam Say <noamsay@gmail.com>
2018-03-06 17:43:05 +0100

    homepage updated with ads

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,7 @@
 class PagesController < ApplicationController
   #skip_before_action :authenticate_user!, only: [:home]
   def home
-    @products = Product.all.page params[:page]
+    #@products = Product.all.page params[:page]
+    @ads = Ad.all.limit(9)
   end
 end
