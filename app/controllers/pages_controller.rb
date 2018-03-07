b7b13d9649dc95f9940903d6062e179a3faeed26
Noam Say <noamsay@gmail.com>
2018-03-07 11:18:47 +0100

    gecoded resuts on homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,12 @@ class PagesController < ApplicationController
   #skip_before_action :authenticate_user!, only: [:home]
   def home
     #@products = Product.all.page params[:page]
+    if @geolocation.first.latitude && @geolocation.first.longitude
+     return @ads = Ad.near([@geolocation.first.latitude, @geolocation.first.longitude], 20).limit(9)
+    end
     @ads = Ad.all.limit(9)
+
+    # @geolocation use .near de Geocoding
+
   end
 end
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -4,3 +4,5 @@ import { testModal } from '../components/adshow'
 
 initUpdateNavbarOnScroll();
 testModal();
+
+
