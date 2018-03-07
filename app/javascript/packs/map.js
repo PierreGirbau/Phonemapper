cf0209fc9d06c8439f6d26b17c8d636d18328c4d
DamienDaube <damien.daube@gmail.com>
2018-03-07 13:11:30 +0100

    Add map style

--- app/javascript/packs/map.js
+++ app/javascript/packs/map.js
@@ -13,4 +13,164 @@ if (mapElement) { // don't try to build a map if there's no div#map to inject in
   } else {
     map.fitLatLngBounds(markers);
   }
+  const styles = [
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#000000"
+            },
+            {
+                "weight": "1.81"
+            },
+            {
+                "gamma": "1.92"
+            },
+            {
+                "saturation": "-3"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative",
+        "elementType": "labels.text.stroke",
+        "stylers": [
+            {
+                "saturation": "-46"
+            },
+            {
+                "lightness": "-33"
+            },
+            {
+                "weight": "3.47"
+            },
+            {
+                "gamma": "1.72"
+            },
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#fafafa"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": "-41"
+            },
+            {
+                "lightness": "13"
+            },
+            {
+                "weight": "1.27"
+            },
+            {
+                "gamma": "0.46"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#11998e"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "color": "#7aaf31"
+            },
+            {
+                "weight": "1.46"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+            {
+                "lightness": "59"
+            },
+            {
+                "saturation": "-63"
+            },
+            {
+                "weight": "1.81"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+            {
+                "lightness": "56"
+            },
+            {
+                "weight": "1.81"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#38ef7d"
+            },
+            {
+                "visibility": "on"
+            },
+            {
+                "weight": "1.29"
+            }
+        ]
+    }
+];
+  map.addStyle({
+    styles: styles,
+    mapTypeId: 'map_style'
+  });
+  map.setStyle('map_style');
 }
